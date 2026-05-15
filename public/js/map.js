178059e0d4b8aff9478aac8fccc68c8db328d564
Diya Baurai <bauraidiya715@gmail.com>
2026-05-15 16:53:30 +0530

    Improve map rendering and marker positioning

--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,23 @@
 mapboxgl.accessToken = mapToken ;
+const coordinates = listing.geometry.coordinates;
 const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v12',
-        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center:coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
 });
 
-const marker = new mapboxgl.Marker()
-    .setLngLat(listing.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 })
-            .setHTML(`
-                <h4>${listing.title}</h4>
-                <p>Exact location provided after Booking</p>
-            `)
-            .setMaxWidth("300px")
-    )
-    .addTo(map);
+map.on("load", () => {
+    map.resize();
+
+    new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .setPopup(
+            new mapboxgl.Popup({ offset: 25 })
+                .setHTML(`
+                    <h4>${listing.title}</h4>
+                    <p>Exact location provided after Booking</p>
+                `)
+        )
+        .addTo(map);
+});
